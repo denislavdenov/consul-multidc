2788086d7a044bfaa975505bb1798baf8f5f20ed
Denov <denov@Denovs-MacBook-Pro.local>
2019-02-08 10:51:51 +0200

    adding code for multi datacenter

---
+++ Vagrantfile
@@ -0,0 +1,44 @@
+SERVER_COUNT = 1
+CONSUL_VER = "1.4.2"
+CT_VER = "0.19.5"
+LOG_LEVEL = "debug" #The available log levels are "trace", "debug", "info", "warn", and "err". if empty - default is "info"
+DC_COUNT = 2
+DOMAIN = "denislav"
+
+
+Vagrant.configure("2") do |config|
+  config.vm.synced_folder ".", "/vagrant", disabled: false
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 512
+    v.cpus = 2
+  
+  end
+  ["sofia", "botevgrad"].to_enum.with_index(1).each do |dcname, dc|
+
+    
+    (1..SERVER_COUNT).each do |i|
+      
+      config.vm.define "consul-server#{i}-#{dcname}" do |node|
+        node.vm.box = "denislavd/xenial64"
+        node.vm.hostname = "consul-server#{i}-#{dcname}"
+        node.vm.provision :shell, path: "scripts/install_consul.sh", env: {"CONSUL_VER" => CONSUL_VER}
+        node.vm.provision :shell, path: "scripts/start_consul.sh", env: {"SERVER_COUNT" => SERVER_COUNT,"LOG_LEVEL" => LOG_LEVEL,"DOMAIN" => DOMAIN,"DCS" => "#{dcname}","DC" => "#{dc}"}
+        node.vm.provision :shell, path: "scripts/keyvalue.sh", env: {"I" => "#{dc}"}
+        node.vm.network "private_network", ip: "10.#{dc}0.56.1#{i}"
+        node.vm.network "forwarded_port", guest: 8500, host: 8500 + dc
+      end
+    end
+
+    config.vm.define "client-nginx1-#{dcname}" do |nginx|
+      nginx.vm.box = "denislavd/nginx64"
+      nginx.vm.hostname = "client-nginx1-#{dcname}"
+      nginx.vm.provision :shell, path: "scripts/install_consul.sh", env: {"CONSUL_VER" => CONSUL_VER}
+      nginx.vm.provision :shell, path: "scripts/start_consul.sh", env: {"SERVER_COUNT" => SERVER_COUNT,"LOG_LEVEL" => LOG_LEVEL,"DOMAIN" => DOMAIN,"DCS" => "#{dcname}","DC" => "#{dc}"}
+      nginx.vm.provision :shell, path: "scripts/consul-template.sh", env: {"CT_VER" => CT_VER}
+      nginx.vm.provision :shell, path: "scripts/check_nginx.sh"
+      nginx.vm.network "private_network", ip: "10.#{dc}0.66.11"
+      nginx.vm.network "forwarded_port", guest: 80, host: 8080 + dc
+    end  
+
+  end
+end
---
+++ scripts/start_consul.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+set -x
+sudo systemctl stop consul
+sleep 5
+sudo systemctl status consul
+DOMAIN=${DOMAIN}
+DCNAME=${DCS}
+DC=${DC}
+var2=$(hostname)
+mkdir -p /vagrant/logs
+mkdir -p /etc/consul.d
+
+
+# Starting consul
+killall consul
+
+LOG_LEVEL=${LOG_LEVEL}
+if [ -z "${LOG_LEVEL}" ]; then
+    LOG_LEVEL="info"
+fi
+
+if [ -d /vagrant ]; then
+  mkdir /vagrant/logs
+  LOG="/vagrant/logs/${var2}.log"
+else
+  LOG="vault.log"
+fi
+
+IP=$(hostname -I | cut -f2 -d' ')
+
+sudo useradd --system --home /etc/consul.d --shell /bin/false consul
+sudo chown --recursive consul:consul /etc/consul.d
+sudo chmod -R 755 /etc/consul.d/
+sudo mkdir --parents /tmp/consul
+sudo chown --recursive consul:consul /tmp/consul
+mkdir -p /tmp/consul_logs/
+sudo chown --recursive consul:consul /tmp/consul_logs/
+
+cat << EOF > /etc/systemd/system/consul.service
+[Unit]
+Description="HashiCorp Consul - A service mesh solution"
+Documentation=https://www.consul.io/
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+User=consul
+Group=consul
+ExecStart=/usr/local/bin/consul agent -config-dir=/etc/consul.d/
+ExecReload=/usr/local/bin/consul reload
+KillMode=process
+Restart=on-failure
+LimitNOFILE=65536
+
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+
+
+if [[ "${var2}" =~ "consul-server" ]]; then
+    killall consul
+    
+    
+    SERVER_COUNT=${SERVER_COUNT}
+    echo $SERVER_COUNT
+    if [[ "${var2}" =~ "sofia" ]]; then
+    #cp /vagrant/config_${DCNAME}.json /etc/consul.d/config_${DCNAME}.json
+    cat << EOF > /etc/consul.d/config_${DCNAME}.json
+    
+    {
+        
+        "server": true,
+        "node_name": "${var2}",
+        "bind_addr": "${IP}",
+        "client_addr": "0.0.0.0",
+        "bootstrap_expect": ${SERVER_COUNT},
+        "retry_join": ["10.10.56.11"],
+        "log_level": "${LOG_LEVEL}",
+        "data_dir": "/tmp/consul",
+        "enable_script_checks": true,
+        "domain": "${DOMAIN}",
+        "datacenter": "${DCNAME}",
+        "ui": true,
+        "disable_remote_exec": true
+
+    }
+
+EOF
+    fi
+
+    if [[ "${var2}" =~ "botevgrad" ]]; then
+    #cp /vagrant/config_${DCNAME}.json /etc/consul.d/config_${DCNAME}.json
+    cat << EOF > /etc/consul.d/config_${DCNAME}.json
+    
+    {
+        
+        "server": true,
+        "node_name": "${var2}",
+        "bind_addr": "${IP}",
+        "client_addr": "0.0.0.0",
+        "bootstrap_expect": ${SERVER_COUNT},
+        "retry_join": ["10.20.56.11"],
+        "retry_join_wan": ["10.10.56.11"],
+        "log_level": "${LOG_LEVEL}",
+        "data_dir": "/tmp/consul",
+        "enable_script_checks": true,
+        "domain": "${DOMAIN}",
+        "datacenter": "${DCNAME}",
+        "ui": true,
+        "disable_remote_exec": true
+
+    }
+
+EOF
+    fi
+
+
+    sleep 1
+    sudo systemctl enable consul
+    sudo systemctl start consul
+    journalctl -f -u consul.service > /vagrant/logs/${var2}.log &
+    sleep 5
+    sudo systemctl status consul
+
+else
+    if [[ "${var2}" =~ "client" ]]; then
+        killall consul
+       
+        cat << EOF > /etc/consul.d/consul_client.json
+
+        {
+            "node_name": "${var2}",
+            "bind_addr": "${IP}",
+            "client_addr": "0.0.0.0",
+            "retry_join": ["10.${DC}0.56.11"],
+            "log_level": "${LOG_LEVEL}",
+            "data_dir": "/tmp/consul",
+            "enable_script_checks": true,
+            "domain": "${DOMAIN}",
+            "datacenter": "${DCNAME}",
+            "ui": true,
+            "disable_remote_exec": true
+        }
+
+EOF
+    fi
+
+    sleep 1
+
+    sudo systemctl enable consul
+    sudo systemctl start consul
+    journalctl -f -u consul.service > /vagrant/logs/${var2}.log &
+    sleep 5
+    sudo systemctl status consul
+    
+fi
+
+
+sleep 5
+consul members
+consul members -wan
+set +x
