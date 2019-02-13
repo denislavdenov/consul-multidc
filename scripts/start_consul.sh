9ce93c5add894011a744803c46ff82906329a313
Denov <denov@Denovs-MacBook-Pro.local>
2019-02-13 16:24:36 +0200

    adding tls code

--- scripts/start_consul.sh
+++ scripts/start_consul.sh
@@ -6,10 +6,35 @@ sudo systemctl status consul
 DOMAIN=${DOMAIN}
 DCNAME=${DCS}
 DC=${DC}
+TLS=${TLS}
+echo ${TLS}
 var2=$(hostname)
 mkdir -p /vagrant/logs
 mkdir -p /etc/consul.d
 
+if [ ${TLS} = true ]; then
+    mkdir -p /etc/tls
+    sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no vagrant@10.10.46.11:"/etc/vault.d/vault.crt" /etc/tls/
+     # Unsealing vault
+
+    curl \
+        --request PUT \
+        --cacert /etc/tls/vault.crt \
+        --data "{ \"key\": \"`cat /vagrant/keys.txt | grep \"Unseal Key 1:\" | cut -c15-`\"}" \
+        https://10.10.46.11:8200/v1/sys/unseal
+
+    curl \
+        --request PUT \
+        --cacert /etc/tls/vault.crt \
+        --data "{ \"key\": \"`cat /vagrant/keys.txt | grep \"Unseal Key 2:\" | cut -c15-`\"}" \
+        https://10.10.46.11:8200/v1/sys/unseal
+
+    curl \
+        --request PUT \
+        --cacert /etc/tls/vault.crt \
+        --data "{ \"key\": \"`cat /vagrant/keys.txt | grep \"Unseal Key 3:\" | cut -c15-`\"}" \
+        https://10.10.46.11:8200/v1/sys/unseal
+fi
 
 # Starting consul
 killall consul
@@ -58,16 +83,53 @@ WantedBy=multi-user.target
 
 EOF
 
+if [ ${TLS} = true ]; then
+    if [[ "${var2}" == "consul-server1-sofia" ]]; then
+        encr=`consul keygen`
+        cat << EOF > /etc/consul.d/encrypt.json
 
+        {
+            "encrypt": "${encr}"
+        }
+EOF
+    fi
+fi
 
 if [[ "${var2}" =~ "consul-server" ]]; then
     killall consul
-    
+    if [ ${TLS} = true ]; then
+        sudo sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no vagrant@10.10.56.11:"/etc/consul.d/encrypt.json" /etc/consul.d/
+        CERTS=`curl --cacert /etc/tls/vault.crt --header "X-Vault-Token: \`cat /vagrant/keys.txt | grep "Initial Root Token:" | cut -c21-\`"        --request POST        --data '{"common_name": "'server.${DCNAME}.${DOMAIN}'", "alt_names": "localhost", "ip_sans": "127.0.0.1", "ttl": "24h"}'       https://10.10.46.11:8200/v1/pki_int/issue/example-dot-com`
+        if [ $? -ne 0 ];then
+        $echo 'There is no certificates received'
+        exit 1
+        fi
+        echo $CERTS | jq -r .data.issuing_ca > /etc/tls/consul-agent-ca.pem
+        echo $CERTS | jq -r .data.certificate > /etc/tls/consul-agent.pem
+        echo $CERTS | jq -r .data.private_key > /etc/tls/consul-agent-key.pem
+        cat << EOF > /etc/consul.d/tls.json
+
+        {
+            "verify_incoming_rpc": true,
+            "verify_incoming_https": false,
+            "verify_outgoing": true,
+            "verify_server_hostname": true,
+            "ca_file": "/etc/tls/consul-agent-ca.pem",
+            "cert_file": "/etc/tls/consul-agent.pem",
+            "key_file": "/etc/tls/consul-agent-key.pem",
+            "ports": {
+                "http": -1,
+                "https": 8501
+            }
+        }
+
+EOF
+    fi
     
     SERVER_COUNT=${SERVER_COUNT}
     echo $SERVER_COUNT
     if [[ "${var2}" =~ "sofia" ]]; then
-    #cp /vagrant/config_${DCNAME}.json /etc/consul.d/config_${DCNAME}.json
+
     cat << EOF > /etc/consul.d/config_${DCNAME}.json
     
     {
@@ -93,7 +155,7 @@ EOF
     fi
 
     if [[ "${var2}" =~ "botevgrad" ]]; then
-    #cp /vagrant/config_${DCNAME}.json /etc/consul.d/config_${DCNAME}.json
+    
     cat << EOF > /etc/consul.d/config_${DCNAME}.json
     
     {
@@ -129,7 +191,34 @@ EOF
 else
     if [[ "${var2}" =~ "client" ]]; then
         killall consul
-       
+        if [ ${TLS} = true ]; then
+            sudo sshpass -p 'vagrant' scp -o StrictHostKeyChecking=no vagrant@10.10.56.11:"/etc/consul.d/encrypt.json" /etc/consul.d/
+            CERTS=`curl --cacert /etc/tls/vault.crt --header "X-Vault-Token: \`cat /vagrant/keys.txt | grep "Initial Root Token:" | cut -c21-\`"        --request POST        --data '{"common_name": "'server.${DCNAME}.${DOMAIN}'", "alt_names": "localhost", "ip_sans": "127.0.0.1", "ttl": "24h"}'       https://10.10.46.11:8200/v1/pki_int/issue/example-dot-com`
+            if [ $? -ne 0 ];then
+            $echo 'There is no certificates received'
+            exit 1
+            fi
+            echo $CERTS | jq -r .data.issuing_ca > /etc/tls/consul-agent-ca.pem
+            echo $CERTS | jq -r .data.certificate > /etc/tls/consul-agent.pem
+            echo $CERTS | jq -r .data.private_key > /etc/tls/consul-agent-key.pem
+            cat << EOF > /etc/consul.d/tls.json
+
+            {
+                "verify_incoming_rpc": true,
+                "verify_incoming_https": false,
+                "verify_outgoing": true,
+                "verify_server_hostname": true,
+                "ca_file": "/etc/tls/consul-agent-ca.pem",
+                "cert_file": "/etc/tls/consul-agent.pem",
+                "key_file": "/etc/tls/consul-agent-key.pem",
+                "ports": {
+                    "http": -1,
+                    "https": 8501
+                }
+            }
+
+EOF
+        fi
         cat << EOF > /etc/consul.d/consul_client.json
 
         {
@@ -161,6 +250,13 @@ fi
 
 
 sleep 5
-consul members
-consul members -wan
+if [ ${TLS} = true ]; then
+    consul members -ca-file=/etc/tls/consul-agent-ca.pem -client-cert=/etc/tls/consul-agent.pem -client-key=/etc/tls/consul-agent-key.pem -http-addr="https://127.0.0.1:8501"
+    consul members -wan -ca-file=/etc/tls/consul-agent-ca.pem -client-cert=/etc/tls/consul-agent.pem -client-key=/etc/tls/consul-agent-key.pem -http-addr="https://127.0.0.1:8501"
+    curl --cacert /etc/tls/vault.crt --header "X-Vault-Token: `cat /vagrant/keys.txt | grep \"Initial Root Token:\" | cut -c21-`" --request PUT https://10.10.46.11:8200/v1/sys/seal
+
+else
+    consul members
+    consul members -wan
+fi
 set +x
